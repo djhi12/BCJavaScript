70a5316a121f03d11e31fa7d399ebffeba82f731
Daniel John Idorot <ido20002@byui.edu>
2023-03-04 15:26:32 +0800

    Initial commit

--- lua/swordFight/stats.lua
+++ lua/swordFight/stats.lua
@@ -1,23 +1,35 @@
-local dataStores = game:GetService("DataStoreService"):GetDataStore("BucksDataStore")
-local defaultCash = 10
-local playersLeft = 0
+local dataStores = game:GetService("DataStoreService"):GetDataStore("BucksDataStore") -- The first line of code creates a variable called "dataStores" and assigns it the value of the DataStoreService's GetDataStore method called "BucksDataStore". This method is used to get a DataStore object with the name "BucksDataStore". The DataStoreService is a Roblox service that allows games to store and retrieve data for players across sessions.
 
-game.Players.PlayerAdded:Connect(function(player)
+local defaultCash = 10 -- The second line of code creates a variable called "defaultCash" and assigns it the value of 10. This is likely a default value for the amount of currency a player has if they have not yet earned any in the game.
+
+local playersLeft = 0 -- The third line of code creates a variable called "playersLeft" and assigns it the value of 0. This variable might be used to keep track of how many players are still active in the game.
+
+game.Players.PlayerAdded:Connect(function(player) -- game.Players refers to a table that holds all the players currently in the game. The PlayerAdded event is fired by Roblox when a new player joins the game. The :Connect() method is used to register a function to be called when this event is fired.
+
+    --[[ 
+        In this case, the function passed to :Connect() takes one argument, player, which represents the player who just joined the game. The code inside the function will be executed every time a new player joins the game.    
+    --]]
 	
-	--playersLeft = playersLeft + 1
+	playersLeft = playersLeft + 1 -- This Lua code increases the value of a variable named "playersLeft" by 1.
 	
-	--to  create a leaderboard
-	local leaderstats = Instance.new("Folder")
-	leaderstats.Name = "leaderstats"--leaderstats should be mentioned like this only, otherwise the leaderboard will not pop up
+
+	local leaderstats = Instance.new("Folder") -- This Lua code creates a new folder object called leaderstats and sets its name to "leaderstats" using the Instance.new() function. The Instance.new() function creates a new instance of a Roblox object, in this case, a folder.
+
+	leaderstats.Name = "leaderstats"
+	leaderstats.Parent = player -- The code then sets the parent of the leaderstats folder to a player object. This means that the leaderstats folder will be a child of the player object in the hierarchy of the Roblox game.
+
+    --[[  
+        This code is often used in games to create a place to store player-specific data such as a player's score or game progression. By creating a leaderstats folder and attaching it to the player object, the game can keep track of each player's progress separately.    
+    --]]
 	
-	--parent the leaderstats folder so that every player will have their own folder called leaderstats
-	leaderstats.Parent = player
-	
-	--create a variable for the currency
-	local bucks = Instance.new("IntValue") --IntValue means integer value since currency will be in the form of numbers
-	bucks.Name = "Bucks"
-	bucks.Value = 0 							--Initally the value will be 0
-	bucks.Parent = leaderstats
+
+	local bucks = Instance.new("IntValue") -- This code creates a new IntValue object called bucks using the Instance.new() function. The IntValue class is used to represent an integer value in Lua.
+
+	bucks.Name = "Bucks" -- Next, the code sets the Name property of bucks to "Bucks".This sets the name of the IntValue object to "Bucks".
+
+	bucks.Value = 0 -- Then, the code sets the Value property of bucks to 0.
+
+	bucks.Parent = leaderstats -- This sets the parent of the bucks object to leaderstats, which is presumably another object in the script.
 	
 	player.CharacterAdded:Connect(function(character)
 		character.Humanoid.WalkSpeed = 16
