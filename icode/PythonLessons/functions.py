f741d9626cf50c73a31a146c6a8dcc08caf2295e
Daniel John Idorot <ido20002@byui.edu>
2023-03-05 18:54:40 +0800

    Initial commit

---
+++ icode/PythonLessons/functions.py
@@ -0,0 +1,13 @@
+def greet(name):
+    """
+    This function takes a name as an argument and returns a greeting string.
+    """
+    return f"Hello, {name}! How are you doing today?"
+
+
+# Example usage:
+print(greet("Alice"))
+# Output: "Hello, Alice! How are you doing today?"
+
+print(greet("Bob"))
+# Output: "Hello, Bob! How are you doing today?"
