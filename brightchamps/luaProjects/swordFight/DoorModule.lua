b930721c1ad93ed278d9600d52d29f1434627295
Daniel John Idorot <ido20002@byui.edu>
2023-03-11 21:20:15 +0800

    Initial commit

--- brightchamps/luaProjects/swordFight/DoorModule.lua
+++ brightchamps/luaProjects/swordFight/DoorModule.lua
@@ -1,32 +1,36 @@
 local module = {}
 
-local tweenService = game:GetService("TweenService")
+local tweenService = game:GetService("TweenService") --[[ The TweenService is a built-in service provided by the Roblox game engine, which allows developers to create smooth transitions between different states or properties of objects in a game. For example, using TweenService, developers can smoothly animate the movement of a character or the scaling of an object over a specified period of time.
 
--- swing the door by `angle` degrees
-function module.swingDoor(angle,door)
-    -- locate the hinge on the door
-    local hingeBefore = door.CFrame * CFrame.new(-2.5, 0, 0)
+In this code, the game:GetService() function is called to retrieve the TweenService object from the game engine. The local keyword is used to declare a variable named tweenService and assign the retrieved TweenService object to it.
 
-    -- relative to the hinge, where is the door?
-    local relative = hingeBefore:toObjectSpace(door.CFrame)
+This code snippet is often used at the beginning of a script that requires the TweenService object, so that it can be easily accessed and used throughout the rest of the script.	
+--]]
 
-    -- twist the hinge
-    -- CONVERT angle *from* degrees *to* radians here
-    local hingeAfter = hingeBefore * CFrame.Angles(0, math.rad(angle), 0)
 
-    -- re-attach the door to the new "hinge"
+function module.swingDoor(angle,door) -- The function takes two arguments: the angle to swing the door by, and a reference to the door object in the game world.
+  
+    local hingeBefore = door.CFrame * CFrame.new(-2.5, 0, 0) -- The code then locates the hinge of the door. It does this by taking the current position and orientation of the door, and then applying a translation (using CFrame.new) to move the hinge location to a fixed offset from the door's origin.
 
-	local tweenProperties = TweenInfo.new(0.2) -- Time
-	local result = {CFrame = hingeAfter:toWorldSpace(relative)}
+   
+    local relative = hingeBefore:toObjectSpace(door.CFrame) -- Next, the code calculates the current position of the door relative to the hinge. It does this by transforming the current position of the door into the coordinate space of the hinge using toObjectSpace.
+
+   
+    local hingeAfter = hingeBefore * CFrame.Angles(0, math.rad(angle), 0) -- The hinge is then rotated around the Y-axis by the specified angle (converted from degrees to radians using math.rad), using the Angles method of the CFrame class.
+
+	local tweenProperties = TweenInfo.new(0.2) -- TweenInfo is used to define the length of the transition (0.2 seconds in this case), and Create is used to create the tween object.
+
+	local result = {CFrame = hingeAfter:toWorldSpace(relative)} -- The door is then repositioned relative to the new hinge location by applying the relative position offset calculated earlier to the new hinge location, and transforming it back into the world coordinate space using toWorldSpace.
 	
-	door.CanCollide = false
+	door.CanCollide = false -- The CanCollide property of the door is set to false, which allows the door to pass through other objects during the tween transition.
 	
-	local tween = tweenService:Create(door,tweenProperties,result)
-	tween:Play()
+	local tween = tweenService:Create(door,tweenProperties,result) -- A Tween is created to smoothly transition the door's position and orientation from its current location to the new position and orientation specified by result. TweenInfo is used to define the length of the transition (0.2 seconds in this case), and Create is used to create the tween object.
+
+	tween:Play() -- The Tween is played, causing the door to smoothly move and rotate to its new position and orientation over the specified duration.
 	
-	wait(0.2)
+	wait(0.2) -- The function then waits for 0.2 seconds (the length of the tween transition) using wait.
 	
-	door.CanCollide = true
+	door.CanCollide = true -- Finally, the CanCollide property of the door is set back to true, causing the door to once again interact with other objects in the game world.
 end
 
 function module.ActivateDoors(doorFolder)
