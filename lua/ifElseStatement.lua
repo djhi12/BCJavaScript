0114e9acbd7da34a333ecd4cc5678d8a030a049b
Daniel John Idorot <ido20002@byui.edu>
2023-03-15 20:02:30 +0800

    Initial commit

---
+++ lua/ifElseStatement.lua
@@ -0,0 +1,25 @@
+--[[
+    In Lua, the if-else statement is used to create conditional statements in your code. The syntax for the if-else statement is as follows:
+--]]
+
+if condition then
+    -- code to execute if the condition is true
+else
+    -- code to execute if the condition is false
+end
+
+-- Here, the condition is an expression that evaluates to either true or false. If the condition is true, then the code inside the if block will be executed. If the condition is false, then the code inside the else block will be executed.
+
+
+-- You can also use multiple conditions using the elseif keyword. Here is an example:
+if condition1 then
+    -- code to execute if condition1 is true
+elseif condition2 then
+    -- code to execute if condition2 is true
+else
+    -- code to execute if both condition1 and condition2 are false
+end
+
+-- In this example, if condition1 is true, then the code inside the first if block will be executed. If condition1 is false and condition2 is true, then the code inside the elseif block will be executed. If both condition1 and condition2 are false, then the code inside the else block will be executed.
+
+
