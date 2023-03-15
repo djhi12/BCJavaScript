e9e80bc6dabb8d537548e6a4b5502ba273aa49f6
Daniel John Idorot <ido20002@byui.edu>
2023-03-16 07:21:07 +0800

    Initial commit

---
+++ icode/JSGolden/square.js
@@ -0,0 +1,11 @@
+function start(){
+    square(4);
+    square(13);
+}
+
+// This function prints out the
+// square of the given value
+function square(x){
+    var squareX = x * x;
+    println(squareX);
+}
