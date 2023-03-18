0ed6437c00c52aca8893d2912ae36072db24eda5
Daniel John Idorot <ido20002@byui.edu>
2023-03-19 02:15:49 +0800

    Initial commit

---
+++ icode/JSKarel/pancakes.js
@@ -0,0 +1,15 @@
+move();
+makePancakes();
+move();
+move();
+makePancakes();
+move();
+move();
+makePancakes();
+move();
+
+function makePancakes(){
+    putBall();
+    putBall();
+    putBall();
+}
