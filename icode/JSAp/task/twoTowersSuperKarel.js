6a69c34ec836ad483c45769424187b31a7766441
Daniel John Idorot <ido20002@byui.edu>
2023-03-18 23:03:52 +0800

    Initial commit

---
+++ icode/JSAp/task/twoTowersSuperKarel.js
@@ -0,0 +1,34 @@
+// This has karel build two towers, with three balls in each tower.
+// Should be the same program with turnRight and turnAround removed.
+function start(){
+    move();
+    buildTower();
+    comeDown();
+    move();
+    move();
+    buildTower();
+    move();
+    turnRight();
+}
+
+// Has karel come down from the top of the tower.
+// Precondition: Karel is facing north
+// Postcondition: Karel is facing east at the bottom of the tower
+function comeDown(){
+    turnAround();
+    move();
+    move();
+    turnLeft();
+}
+
+// This builds a three ball tower.
+// Precondition: Karel is at base of tower facing east
+// Postcondition: Karel has laid three balls, facing north
+function buildTower(){
+    turnLeft();
+    putBall();
+    move();
+    putBall();
+    move();
+    putBall();
+}
