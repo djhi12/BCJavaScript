2d54556c1e7d214f847c78c638b368fb22724ff7
Daniel John Idorot <ido20002@byui.edu>
2023-03-20 07:33:09 +0800

    Initial commit

---
+++ icode/JSGolden/forLoop.js
@@ -0,0 +1,30 @@
+/*
+    In JavaScript, a for loop is a control flow statement that allows you to execute a block of code repeatedly for a fixed number of times or until a certain condition is met.
+*/
+
+// The basic syntax of a for loop in JavaScript is:
+for (initialization; condition; increment) {
+    // code to be executed
+}
+
+/*
+  Here's what each part of the syntax means:
+
+    INITIALIZATION is where you declare and initialize any variables that will be used in the loop.
+
+    CONDITION is an expression that is evaluated before each iteration of the loop. If this expression is true, the loop continues; if it's false, the loop terminates.
+
+    INCREMENT is where you update the value of the loop variable at the end of each iteration.
+*/
+
+// Example Code
+for (let i = 0; i < 3; i++) {
+    console.log("Daniel");
+}
+
+/* This will will print my name 3 time 
+    Daniel
+    Daniel
+    Daniel
+
+*/
