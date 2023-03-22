ed86ecb6f98751e5b913ef2ad7ed0fc9683d4471
Daniel John Idorot <ido20002@byui.edu>
2023-03-23 06:34:09 +0800

    Initial commit

---
+++ icode/JSGolden/triple.js
@@ -0,0 +1,11 @@
+function start(){
+    triple(4);
+    triple(8);
+}
+
+// This function prints out triple
+// the given value
+function triple(x){
+    var tripleX = 3 * x;
+    println(tripleX);
+}
