337e3d6c2e70c1ffbeae8ee73324c9d7332f6094
Daniel John Idorot <ido20002@byui.edu>
2023-03-23 06:47:17 +0800

    Initial commit

---
+++ icode/JSGolden/areaOfTriangle.js
@@ -0,0 +1,11 @@
+function start(){
+    printTriangleArea(5, 4);
+    printTriangleArea(6, 8);
+}
+
+// This function prints out the area of
+// a triangle given its base and height
+function printTriangleArea(base, height){
+    var area = (base * height) / 2;
+    println(area);
+}
