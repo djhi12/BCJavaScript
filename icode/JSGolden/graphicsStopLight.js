1f9f2d66a8d0fed0c24e335554656859b4c2b795
Daniel John Idorot <ido20002@byui.edu>
2023-03-30 07:03:28 +0800

    Initial commit

--- icode/JSGolden/graphicsStopLight.js
+++ icode/JSGolden/graphicsStopLight.js
@@ -1,3 +1,4 @@
+// Variables
 var LIGHT_RADIUS = 35;
 var STOPLIGHT_WIDTH = 120;
 var STOPLIGHT_HEIGHT = 350;
@@ -5,21 +6,25 @@ var DIST_BETWEEN_LIGHTS = 100;
 var GRAY_COLOR = "#737071";
 
 function start() {
-    var centerX = getWidth() / 2;
+    // Chamge location
+    var centerX = getWidth() / 2; 
     var centerY = getHeight() / 2;
 	
+    // Make a rectangle 
     var rect = new Rectangle(STOPLIGHT_WIDTH, STOPLIGHT_HEIGHT);
     rect.setColor(GRAY_COLOR);
     rect.setPosition(centerX - STOPLIGHT_WIDTH / 2, centerY - STOPLIGHT_HEIGHT / 2);
     add(rect);
 	
+    // Calling the drawCircle function | red, yellow, and green colors
     drawCircle(LIGHT_RADIUS, Color.red, centerX, centerY - DIST_BETWEEN_LIGHTS);
     drawCircle(LIGHT_RADIUS, Color.yellow, centerX, centerY);
     drawCircle(LIGHT_RADIUS, Color.green, centerX, centerY + DIST_BETWEEN_LIGHTS);
 }
 
-
+// Function circle
 function drawCircle(radius, color, x, y) {
+    // Create a circle
     var circle = new Circle(radius);
     circle.setColor(color);
     circle.setPosition(x, y);
