edd1fe56962fadf1589fbef330305f03b78c70b2
Daniel John Idorot <ido20002@byui.edu>
2023-04-12 09:10:26 +0800

    Initial commit

--- pythonLessons/test.py
+++ pythonLessons/test.py
@@ -1,36 +1,18 @@
-# # print("Test")
-# # This function takes a temperature
-# # in Celcius and converts it to
-# # Farenheit.
-# def celcius_to_farenheit(celcius):
-#     return celcius * 1.8 + 32
+"""
+    When you go out to eat, you always tip 20% of the bill amount. But who’s got the time to calculate the right tip amount every time? Not you that’s for sure! You’re making a program to calculate tips and save some time.
 
-# # This function takes a temperature
-# # in Farenheit and converts it to
-# # Celcius.
-# def farenheit_to_celcius(farenheit):
-#     return (farenheit - 32) / 1.8
+    Your program needs to take the bill amount as input and output the tip as a float.
 
-# try:
-#     c = float(input("Enter a temp in C: "))
-#     print("In F: " + str(celcius_to_farenheit(c)))
-    
-#     f = float(input("Enter a temp in F: "))
-#     print("In C: " + str(farenheit_to_celcius(f)))
-    
-# except ValueError:
-#     print("You must enter a float!")
+    Sample Input
 
+    50
 
+    Sample Output
 
-def retrieve_positive_number():
-    while True:
-        try:
-            number = int(input("Enter a positive number: "))
-            if number > 0:
-                return number # true
-            print("The number must be positive!") # false
-        except ValueError:
-            print("That wasn't a number!")
+    10.0
+"""
 
-print(retrieve_positive_number())
+bill = int(input())
+tip_cal = bill * .20
+tip_amount = float(tip_cal)
+print(tip_amount)
