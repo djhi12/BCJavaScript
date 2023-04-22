89ee5f79cfb089db5e34968da39da4c1c2c68277
Daniel John Idorot <ido20002@byui.edu>
2023-04-23 02:55:12 +0800

    Initial commit

---
+++ icode/JSKarel/decorateFence.js
@@ -0,0 +1,26 @@
+function start() {
+    goToWall();
+    turnLeft();
+    decorateWall();
+}
+
+// Move along the wall
+// Put a ball where there isn't a door or gate
+function decorateWall() {
+    while (frontIsClear()) {
+        if (rightIsBlocked()) {
+            putBall();
+        }
+        move();
+    }
+    if (rightIsBlocked()) {
+        putBall();
+    }
+}
+
+// Go until Karel hits a wall
+function goToWall() {
+    while (frontIsClear()) {
+        move();
+    }
+}
