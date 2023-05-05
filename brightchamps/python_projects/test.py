30a263e85c96e834a126969fdce2e2d38368c5cd
Daniel John Idorot <ido20002@byui.edu>
2023-05-06 05:02:04 +0800

    Initial commit

--- brightchamps/python_projects/test.py
+++ brightchamps/python_projects/test.py
@@ -1,8 +1,15 @@
-vote = input("Are you eligible (y/n)? ")
+# Example program using "in" keyword
 
-if vote == "y":
-    print("Awesome!")
-elif vote == "n":
-    print("Sorry")
+# Define a list of fruits
+fruits = ["apple", "banana", "cherry"]
+
+# Check if "banana" is in the list
+if "banana" in fruits:
+    print("Yes, banana is in the list of fruits!")
+
+# Check if "orange" is in the list
+if "orange" in fruits:
+    print("Yes, orange is in the list of fruits!")
 else:
-    print("Try another next year!")
+    print("No, orange is not in the list of fruits.")
+
