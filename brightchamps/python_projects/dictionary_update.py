fd457d28fbb2a85368bef0f90834118065d6be66
Daniel John Idorot <ido20002@byui.edu>
2023-05-11 15:01:07 +0800

    Initial commit

--- brightchamps/python_projects/dictionary_update.py
+++ brightchamps/python_projects/dictionary_update.py
@@ -1,13 +1,13 @@
 dictionary = {}
 
 def add_word():
-    word = input("Enter a new word: ")
+    word = input("\nEnter a new word: ")
     meaning = input("Enter the meaning of the word: ")
     dictionary[word] = meaning
-    print(f"{word} has been added to the dictionary with meaning: {meaning}")
+    print(f"{word} has been added to the dictionary with meaning: {meaning}\n")
 
 def update_word():
-    word = input("Enter a word to update: ")
+    word = input("\nEnter a word to update: ")
     if word in dictionary:
         new_meaning = input(f"Enter the new meaning of {word}: ")
         dictionary[word] = new_meaning
@@ -16,28 +16,31 @@ def update_word():
         print(f"{word} is not in the dictionary")
 
 while True:
-    print("Select an option:")
+    print("\nSelect an option:")
     print("1. Add a new word")
     print("2. Search for a word")
     print("3. Update an existing word")
     print("4. Exit")
-    choice = input("Enter your choice (1, 2, 3, or 4): ")
+    choice = input("\nEnter your choice (1, 2, 3, or 4): ")
     
     if choice == "1":
         add_word()
     elif choice == "2":
-        word = input("Enter a word to search for: ")
+        word = input("\nEnter a word to search for: ")
         if word in dictionary:
             print(f"The meaning of {word} is: {dictionary[word]}")
         else:
             print(f"{word} is not in the dictionary")
+            
     elif choice == "3":
         update_word()
+        
     elif choice == "4":
-        print("Goodbye!")
+        print("Goodbye!\n")
         break
+    
     else:
-        print("Invalid choice, please try again")
+        print("\nInvalid choice, please try again")
 
 """
     In this updated version of the program, we've added a new function called update_word(). When the user selects option 3, the program prompts them to enter a word to update. If the word is in the dictionary, the program prompts the user to enter the new meaning of the word, and then updates the dictionary with the new meaning using the dictionary[word] = new_meaning syntax.
