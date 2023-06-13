e72ea7f5be0152e823e614628855e05e28d4f626
Daniel John Idorot <ido20002@byui.edu>
2023-06-13 20:08:51 +0800

    Initial commit

---
+++ icode/summerCamp/Lua/IntroToLua.lua
@@ -0,0 +1,59 @@
+--[[
+Title: Introduction to Lua Programming
+
+Objective:
+
+Introduce campers to the basics of Lua programming language.
+Teach campers how to write simple Lua programs.
+Familiarize campers with the syntax and concepts of Lua programming.
+
+Duration: 1 week (3 hours each day)
+
+Materials Needed:
+Computers or laptops with Lua programming environment installed
+Projector or large screen for demonstrations
+Printed handouts with code snippets and exercises
+
+
+Day 1: Introduction to Lua Programming (1 hour)
+Introduction to Lua programming language and its applications.
+Explanation of Lua syntax, variables, and data types.
+Demonstration of basic Lua program structure.
+Guided hands-on exercise: Writing and running a simple "Hello, World!" program.
+Day 2: Control Structures and Loops (1 hour)
+
+Overview of control structures in Lua (if-else, while, for).
+Explanation of conditional statements and logical operators.
+Demonstration of control structures with example programs.
+Guided hands-on exercise: Writing a program to check if a number is even or odd using if-else statements.
+Day 3: Functions and Tables (1 hour)
+
+Introduction to functions in Lua and their importance.
+Explanation of function definition, parameters, and return values.
+Demonstration of functions with example programs.
+Introduction to tables as a data structure in Lua.
+Guided hands-on exercise: Writing a program to calculate the factorial of a number using a recursive function.
+Day 4: File Input/Output (1 hour)
+
+Explanation of file handling in Lua.
+Overview of file modes (read, write, append).
+Demonstration of file input/output operations.
+Guided hands-on exercise: Writing a program to read data from a file and perform simple operations on it.
+Day 5: Mini Project (1 hour)
+
+Recap of concepts learned during the week.
+Campers will work on a mini project individually or in pairs.
+Project options: creating a simple text-based game, implementing a calculator, or designing a basic inventory management system.
+Encourage creativity and problem-solving skills.
+Note: Throughout the week, allocate time for questions and provide assistance to campers as needed. Encourage campers to experiment with the concepts learned and explore additional features of Lua programming.
+
+Evaluation:
+
+Assess campers' understanding of Lua programming through observation, participation, and completion of exercises.
+Evaluate the mini projects based on creativity, functionality, and adherence to programming principles.
+Additional Tips:
+
+Provide extra resources such as online tutorials or Lua documentation for campers interested in further exploring the language.
+Encourage collaboration and peer learning among campers.
+Celebrate achievements and progress at the end of the week.    
+--]]
