f279493704808abf25c0dcfcd3e17bdc1734256e
Daniel John Idorot <ido20002@byui.edu>
2023-06-19 03:47:04 +0800

    Initial commit

--- python_lessons/test.py
+++ python_lessons/test.py
@@ -1,3 +1,12 @@
-letter = ["a", "b", "c", "a"]
 
+person = {}
 
+name = input("Type name: ")
+age = int(input("Type age: "))
+email_address = input("Type email address: ")
+
+person['name'] = name
+person['age'] = age
+person['email_address'] = email_address
+
+print(person)
