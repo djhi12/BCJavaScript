e1b5bc1843bf0b239d51d26c0db4bbdc19bb5dab
Daniel John Idorot <ido20002@byui.edu>
2023-06-20 12:31:14 +0800

    Initial commit

---
+++ python_lessons/errors.py
@@ -0,0 +1,4 @@
+# Missing function or method docstring
+"""
+A docstring is a string that provides documentation for a function or method. It is typically placed as the first line after the function or method definition and is enclosed in triple quotes (\""").
+"""
