a495c7e91233b6740d507a12a90496de8b98414d
Daniel John Idorot <ido20002@byui.edu>
2023-07-14 15:02:19 +0800

    Initial commit

--- data_visualization/test.py
+++ data_visualization/test.py
@@ -1,24 +1,5 @@
-import numpy as np
-
-a = np.array([1, 2, 3])
-b = np.array([4, 5, 6])
-
-# Element-wise addition
-c = a + b
-print(c)
-# Output: [5 7 9]
-
-# Element-wise multiplication
-d = a * b
-print(d)
-# Output: [4 10 18]
-
-# Matrix multiplication
-e = np.dot(a, b)
-print(e)
-# Output: 32
-
-# Mathematical functions
-f = np.sin(a)
-print(f)
-# Output: [0.84147098 0.90929743 0.14112001]
+import csv
+with open('life-expectancy.csv') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
