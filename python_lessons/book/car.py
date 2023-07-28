801bbeafa4d8c036ad7b4376b246f12fd883ff1d
djhi12 <ido20002@byui.edu>
2023-07-28 16:23:29 +0800

    Initial commit

--- python_lessons/book/car.py
+++ python_lessons/book/car.py
@@ -29,6 +29,24 @@ class Car:
     def increment_odometer(self, miles):
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
+        
+class ElectricCar(Car):   
+    """Represent aspects of a car, specific to electric vehicles."""    
+    def __init__(self, make, model, year):        
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery_size = 40
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+        my_leaf = ElectricCar('nissan', 'leaf', 2024)
+        
+        print(my_leaf.get_descriptive_name())
+        my_leaf.describe_battery()
 
 
 
