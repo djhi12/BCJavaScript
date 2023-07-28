0b6a4d2e30376350df63562ca5a17e0d95d3d0fc
djhi12 <ido20002@byui.edu>
2023-07-28 14:06:32 +0800

    Initial commit

--- data_visualization/life_expectancy_year.py
+++ data_visualization/life_expectancy_year.py
@@ -50,3 +50,43 @@ with open(filename) as life_expectancies:
 
 print(
     f"\nThe highest life expectancy in {year_input} is {highest_life_expectancy} years in {country_with_highest_life}.")
+
+
+
+# Lowest Life Exectancy
+
+# filename = 'life-expectancy.csv'
+# year_input = input("Enter a year: ")
+
+# # Initialize the variables to store the highest and lowest life expectancies
+# highest_life_expectancy = 0.0
+# lowest_life_expectancy = float('inf')  # Set it to positive infinity initially
+# # Variables to store the countries with the highest and lowest life expectancies
+# country_with_highest_life = ''
+# country_with_lowest_life = ''
+
+# with open(filename) as life_expectancies:
+#     reader = csv.reader(life_expectancies)
+#     next(reader)  # Skip the header row
+
+#     for row in reader:
+#         country = row[0]
+#         code = row[1]
+#         year = row[2]
+#         life_expectancy = float(row[3])
+
+#         if year == year_input:
+#             # Check for highest life expectancy
+#             if life_expectancy > highest_life_expectancy:
+#                 highest_life_expectancy = life_expectancy
+#                 country_with_highest_life = country
+
+#             # Check for lowest life expectancy
+#             if life_expectancy < lowest_life_expectancy:
+#                 lowest_life_expectancy = life_expectancy
+#                 country_with_lowest_life = country
+
+# print(
+#     f"\nThe highest life expectancy in {year_input} is {highest_life_expectancy} years in {country_with_highest_life}.")
+# print(
+#     f"The lowest life expectancy in {year_input} is {lowest_life_expectancy} years in {country_with_lowest_life}.")
